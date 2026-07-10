d59825279e2ebfa242736fb0a3893aa1fc213859
ejabbing <ernestjabea@gmail.com>
2026-07-10 11:46:19 +0100

    fix: implement FilamentUser interface to allow backoffice access in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,14 +3,24 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser;
+use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Determine if the user can access the Filament panel.
+     */
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return app()->environment('local') || str_ends_with($this->email, '@koriassetmanagement.com');
+    }
 
     /**
      * The attributes that are mass assignable.
